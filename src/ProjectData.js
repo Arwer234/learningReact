a735b255ab81bfa67ae73a11d055f7e92a1065f8
Arwer234 <Arwer234@users.noreply.github.com>
2020-05-12 20:24:19 +0200

    data fix for links

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -4,49 +4,49 @@ export const ProjectData = {
             id:0,
             name:"Lego",
             src: "/lego.png",
-            page:"lego.jfilipowski.pl",
+            page:"http://lego.jfilipowski.pl",
             description: "Project based on three.js, which enables you to have fun building different structures with your friends. Changes you made are sent to server via websockets, then it sends to other clients the data."
         },
         {
             id:1,
             name:"Checkers",
             src:"/checkers.png",
-            page:"checkers.jfilipowski.pl",
+            page:"http://checkers.jfilipowski.pl",
             description:"Typical multiplayer board game created with three.js. Server is responsible for sending the current board status. Gaining points currently under developement."
         },
         {
             id:2,
             name:"Hex",
             src:"/hex_1.png",
-            page:"hex.jfilipowski.pl",
+            page:"http://hex.jfilipowski.pl",
             description:"The most complex gameish project I created so far. At first, you can create your own map layout, select which hexes will contain certain elements. Later, you can enter three.js generated map, where you can move your hero and interact with another objects."
         },
         {
             id:3,
             name:"Click'n'slide",
             src:"/clicknslide.png",
-            page:"projects.jfilipowski.pl/clicknslide",
+            page:"http://projects.jfilipowski.pl/clicknslide",
             description:"Simple plain JS game, where you can put the pieces of images into correct picture. Your best times are kept in cookies."
         },
         {
             id:4,
             name:"Battleships",
             src:"/statki.png",
-            page:"projects.jfilipowski.pl/statki",
+            page:"http://projects.jfilipowski.pl/statki",
             description:"My first game in JS. The rules are simple - put your ships on the map, shoot all the enemy ships."
         },
         {
             id:5,
             name:"MP3 Player",
             src:"/mp3.png",
-            page:"mp3.jfilipowski.pl",
+            page:"http://mp3.jfilipowski.pl",
             description:"Simple player, where you can choose from different albums and songs to listen. Whole app is created via mix of JQuery and plain NodeJS."
         },
         {
             id:6,
             name:"Dratewka",
             src:"/dratewka.png",
-            page:"projects.jfilipowski.pl/dratewka",
+            page:"http://projects.jfilipowski.pl/dratewka",
             description:"The originator of this game in web language is Maciej Mendela, my former JS teacher. It's an JavaScript RPG game, your goal is to kill the dragon with materials you find. It's a port from Atari console, renowned in 90's in Poland."
         }
     ]
