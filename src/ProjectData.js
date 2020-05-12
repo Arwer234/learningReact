36d4378eb77c2a75f3bddffc913ae0448109c640
Arwer234 <Arwer234@users.noreply.github.com>
2020-05-12 20:04:33 +0200

    updated links to pages

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -18,30 +18,35 @@ export const ProjectData = {
             id:2,
             name:"Hex",
             src:"/hex_1.png",
+            page:"hex.jfilipowski.pl",
             description:"The most complex gameish project I created so far. At first, you can create your own map layout, select which hexes will contain certain elements. Later, you can enter three.js generated map, where you can move your hero and interact with another objects."
         },
         {
             id:3,
             name:"Click'n'slide",
             src:"/clicknslide.png",
+            page:"projects.jfilipowski.pl/clicknslide",
             description:"Simple plain JS game, where you can put the pieces of images into correct picture. Your best times are kept in cookies."
         },
         {
             id:4,
             name:"Battleships",
             src:"/statki.png",
+            page:"projects.jfilipowski.pl/statki",
             description:"My first game in JS. The rules are simple - put your ships on the map, shoot all the enemy ships."
         },
         {
             id:5,
             name:"MP3 Player",
             src:"/mp3.png",
+            page:"mp3.jfilipowski.pl",
             description:"Simple player, where you can choose from different albums and songs to listen. Whole app is created via mix of JQuery and plain NodeJS."
         },
         {
             id:6,
             name:"Dratewka",
             src:"/dratewka.png",
+            page:"projects.jfilipowski.pl/dratewka",
             description:"The originator of this game in web language is Maciej Mendela, my former JS teacher. It's an JavaScript RPG game, your goal is to kill the dragon with materials you find. It's a port from Atari console, renowned in 90's in Poland."
         }
     ]
