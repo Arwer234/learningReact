596630cac4fcf2e18fcffcc4df1925da205c3d6c
Arwer234 <Arwer234@users.noreply.github.com>
2020-09-15 10:39:37 +0200

    redirects

--- src/Project.js
+++ src/Project.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import "./Project.css"
 
 export default class Project extends Component {
+    redirect = () =>{
+        window.open(this.props.data.page)
+    }
     render() {
         return (
-            <figure className = "projectTile">
-                <img className = "projectImg"src = {this.props.src[this.props.data.id]} alt = "/"/>
+            <figure onClick = {this.redirect} className = "projectTile">
+                <img className = "projectImg" src = {this.props.src[this.props.data.id]} alt = "/"/>
                 <figcaption className = "projectCaption">{this.props.data.description}</figcaption>
             </figure>
         );
